a4ab5c8578fa9456ff0fd618a8dd28ab48a83fd2
Paweł <pawelgr7@gmail.com>
2020-06-27 11:46:18 +0200

    ver.1.3

--- app/src/main/java/com/example/grybos/dreamcar/CustomAdapter.java
+++ app/src/main/java/com/example/grybos/dreamcar/CustomAdapter.java
@@ -1,4 +1,64 @@
 package com.example.grybos.dreamcar;
 
-public class CustomAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    private ArrayList<String> _list;
+    private Context _context;
+    private int _resource;
+
+    public CustomAdapter(@NonNull Context context, int resource, @NonNull ArrayList objects) {
+        super(context, resource, objects);
+
+        this._list = objects;
+        this._context = context;
+        this._resource = resource;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(_resource, null);
+
+        ImageView image = (ImageView) convertView.findViewById(R.id.image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ImageView delete = (ImageView) convertView.findViewById(R.id.delete);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ImageView edit = (ImageView) convertView.findViewById(R.id.edit);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return convertView;
+
+    }
 }
--- app/src/main/java/com/example/grybos/dreamcar/MainActivity.java
+++ app/src/main/java/com/example/grybos/dreamcar/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.grybos.dreamcar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //zmienne
+    private ListView listView;
+    private ImageView add;
+    private ArrayList<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        listView = findViewById(R.id.list1);
+        add = findViewById(R.id.add);
+
+        CustomAdapter adapter = new CustomAdapter(
+                MainActivity.this,
+                R.layout.listview_layout,
+                list
+        );
+        listView.setAdapter(adapter);
 
     }
 }
